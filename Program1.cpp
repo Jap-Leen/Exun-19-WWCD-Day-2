4604778a2497a90fd9e051c4c9dbe8419307bbb3
muskanvaswan <42072661+muskanvaswan@users.noreply.github.com>
2019-10-20 11:18:07 +0530

    Update Program1.cpp
    a case in which the user enters n=0 must be considered and hence n needs to be equal to 0 for the first element and so on...
    also note that there needs to be another case designating the third element as 1, as there aren't enough elements preceding it.

--- Program1.cpp
+++ Program1.cpp
@@ -1,11 +1,13 @@
 // Tribonacci Series - 0, 0, 1, 1, 2, 4...
 // Complexity: E
-
+//for refference n=0, for first element (this ensures accuracy)
 int tribo(int n){
+        if(n==0)
+        return 0;
         if(n == 1)
         return 0;
         if(n == 2)
-        return 0;
+        return 1;
         return tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
 }
 
