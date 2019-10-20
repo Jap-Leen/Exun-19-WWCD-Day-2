00912b2e6b25538de029c8c2d4d98a095dcfeb82
mehar-goenka <56768224+mehar-goenka@users.noreply.github.com>
2019-10-20 09:37:37 +0530

    Update Program1.py

--- Program1.py
+++ Program1.py
@@ -2,9 +2,11 @@
 # Complexity: H
 def tribo(n):
         if n == 1:
-        return 0;
+                return 0
         if n == 2:
-        return 0;
-        return tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
+                return 0
+        if n == 3:
+               return 1
+        return tribo(n - 1) + tribo(n - 2) + tribo(n - 3)
 
 
