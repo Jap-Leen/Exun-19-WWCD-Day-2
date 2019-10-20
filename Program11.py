b02f04f91db2dc6972157125bcf4ab3864811176
Nikita Agarwal <56750668+nikita206@users.noreply.github.com>
2019-10-20 09:51:18 +0530

    Program11.py

--- Program11.py
+++ Program11.py
@@ -5,7 +5,7 @@ a= '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 str = "Hello!!!, he said ---and went."
 p= ""
 for char in str:
-   if char in a: 
+   if char in a and char!=".":  
        p = p+ char
 print(p)
 
